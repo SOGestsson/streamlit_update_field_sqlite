a8dc6e99d6b73b7c8b8f6f632e0502ec79dd7686
SOGestsson <siggi@rhino-aviation.com>
2024-09-14 17:23:23 +0000

    síðasta útgáfa áður en breytt er í pysql

--- dockerfile
+++ dockerfile
@@ -1,13 +1,43 @@
+# Use the official Python 3.9 slim image as the base
 FROM python:3.9-slim
 
+# Set the working directory in the container
 WORKDIR /app
 
+# Copy the current directory contents into the container at /app
 COPY . .
 
+# Install necessary packages and setup Microsoft SQL Server ODBC driver repository for Debian 11 (Bullseye)
+RUN apt-get update && \
+    apt-get install -y --no-install-recommends \
+    iputils-ping \
+    curl \
+    gnupg2 \
+    apt-transport-https \
+    unixodbc \
+    unixodbc-dev \
+    libgssapi-krb5-2 && \
+    curl -fsSL https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor -o /usr/share/keyrings/microsoft-archive-keyring.gpg && \
+    echo "deb [signed-by=/usr/share/keyrings/microsoft-archive-keyring.gpg] https://packages.microsoft.com/debian/11/prod bullseye main" | tee /etc/apt/sources.list.d/mssql-release.list
+
+# Install the ODBC Driver 18 without mssql-tools
+RUN apt-get update && \
+    ACCEPT_EULA=Y apt-get install -y msodbcsql18 && \
+    echo 'export PATH="$PATH:/opt/mssql-tools/bin"' >> ~/.bashrc && \
+    . ~/.bashrc
+
+# Clean up APT when done to keep the image size down
+RUN apt-get clean && \
+    rm -rf /var/lib/apt/lists/*
+
+# Install Python dependencies
 RUN pip3 install -r requirements.txt
 
+# Expose port 8501 to the outside world
 EXPOSE 8501
 
-HEALTHCHECK CMD curl --fail http://localhost:8501/_stcore/health
+# Define the healthcheck for the container
+HEALTHCHECK CMD curl --fail http://localhost:8501/_stcore/health || exit 1
 
+# Run the streamlit application
 ENTRYPOINT ["streamlit", "run", "app.py", "--server.port=8501", "--server.address=0.0.0.0"]
--- models/orm_models.py
+++ models/orm_models.py
@@ -1,17 +1,24 @@
 
 import os
+import urllib.parse
+import pyodbc
 from sqlalchemy import create_engine, Column, Integer, String, Date
 from sqlalchemy.orm import declarative_base, sessionmaker
 
-db_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), '..', 'rio.db')
-connection_string = f'sqlite:///{db_path}'
 
-engine = create_engine(connection_string)
+# Correct the params line using urllib.parse.quote_plus
+params = urllib.parse.quote_plus("DRIVER={ODBC Driver 18 for SQL Server};SERVER=10.80.61.10;DATABASE=rio;UID=riouser;PWD=phU$E5ib!3Zanl*0s$t5;TrustServerCertificate=yes;")
 
+# Create the engine
+engine = create_engine("mssql+pyodbc:///?odbc_connect=%s" % params, fast_executemany=True, connect_args={'connect_timeout': 10}, echo=False)
+
+# Define the base class for models
 Base = declarative_base()
 
 class RioItems(Base):
-    __tablename__ = 'rio_items'
+    __tablename__ = 'rio_item_editables'
+    __table_args__ = {'schema': 'prod'}  # Specify the schema
+
     id = Column(Integer, primary_key=True)
     item_number = Column(String, nullable = False)
     description = Column(String, nullable = False)
@@ -22,4 +29,17 @@ class RioItems(Base):
 
 # Create a configured "Session" class
 Session = sessionmaker(bind=engine)
+
+# Create an instance of the session
+session = Session()
+
+# Perform the query using the session instance
+query = session.query(RioItems).filter(RioItems.innkaupum_haett == 0)
+
+# Example: Fetch all matching records
+results = query.all()
+
+# Print the results
+for item in results:
+    print(f"id: {item.id}, Item Number: {item.item_number}, Description: {item.description}, Buy_Freq:{item.buy_freq}")
 
